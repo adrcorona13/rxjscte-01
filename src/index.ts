457a1001fa472fae88410fac7f7879878c4c3d4e
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-29 15:06:31 -0600

    crear subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
+const observer: Observer<any> = {
+    next: v => console.log('next: ', v),
+    error: e => console.error('error: ', e),
+    complete: () => console.info('complete'),
+}
 
 // const obs$ = Observable.create();
 const obs$ = new Observable<string>(subscriber => {
     subscriber.next('Hola');
     subscriber.next('Mundo');
-    subscriber.next('Hola');
-    subscriber.next('Mundo');
-    subscriber.next('Mundo');
+
+    // throw ('Error');
+
     subscriber.complete();
 });
 
-obs$.subscribe(console.log);
+// obs$.subscribe(console.log);
+// obs$.subscribe(resp => {
+//     console.log(resp);
+// });
+// obs$.subscribe({
+//     next: (v) => console.log('next: ', v),
+//     error: (e) => console.error('error: ', e),
+//     complete: () => console.log('complete')    
+// });
+
+obs$.subscribe(observer);
+
+obs$.subscribe()
