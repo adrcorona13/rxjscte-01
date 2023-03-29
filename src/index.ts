f7983fbdb9a6307c422aa5199c93f24e16a7e910
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-29 14:52:21 -0600

    creacion de un observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+import { Observable } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+// const obs$ = Observable.create();
+const obs$ = new Observable<string>(subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+    subscriber.next('Mundo');
+    subscriber.complete();
+});
 
-
-
-
-
-
-
+obs$.subscribe(console.log);
